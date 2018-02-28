995124230ce61460fd40217fe7888438d3115e14
233moutian <1107022342@qq.com>
2018-02-28 23:11:23 +0800

    实体类设计及mapper.xml填充

--- src/main/java/com/aode/dao/BlogMapper.java
+++ src/main/java/com/aode/dao/BlogMapper.java
@@ -1,12 +1,11 @@
-//package com.aode.dao;
-//
-//import com.aode.model.POJO.Blog;
-//import com.aode.util.BaseMapper;
-//
-///**
-// * Created by moutian on 2018/2/28 0028.
-// */
-////@Repository
-//public interface BlogMapper extends BaseMapper<Blog> {
-//
-//}
+package com.aode.dao;
+
+import com.aode.model.POJO.Blog;
+import com.aode.util.BaseMapper;
+
+/**
+ * Created by moutian on 2018/2/28 0028.
+ */
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}
--- src/main/java/com/aode/dao/CommentMapper.java
+++ src/main/java/com/aode/dao/CommentMapper.java
@@ -1,11 +1,11 @@
-//package com.aode.dao;
-//
-//import com.aode.model.POJO.Comment;
-//import com.aode.util.BaseMapper;
-//
-///**
-// * Created by moutian on 2018/2/28 0028.
-// */
-////@Repository
-//public interface CommentMapper extends BaseMapper<Comment> {
-//}
+package com.aode.dao;
+
+import com.aode.model.POJO.Comment;
+import com.aode.util.BaseMapper;
+
+/**
+ * Created by moutian on 2018/2/28 0028.
+ */
+public interface CommentMapper extends BaseMapper<Comment> {
+
+}
