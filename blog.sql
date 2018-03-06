2088d8a085499e8df80779ad410d5ef16a6581e1
233moutian <1107022342@qq.com>
2018-03-06 15:55:41 +0800

    sql文件补充

--- blog.sql
+++ blog.sql
@@ -56,3 +56,42 @@ CREATE TABLE `blog_user` (
 -- ----------------------------
 -- Records of gw_user
 -- ----------------------------
+
+DROP TABLE IF EXISTS `blog_blog`;
+CREATE TABLE `blog_blog` (
+  `b_id` varchar(255) NOT NULL DEFAULT '',
+  `a_id` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `summary` varchar(255) DEFAULT NULL,
+  `release_date` datetime DEFAULT NULL,
+  `click_hit` int(11) DEFAULT NULL,
+  `reply_hit` int(11) DEFAULT NULL,
+  `content` text,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`b_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of blog_blog
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `blog_comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `blog_comment`;
+CREATE TABLE `blog_comment` (
+  `c_id` varchar(255) NOT NULL DEFAULT '',
+  `b_id` varchar(255) DEFAULT NULL,
+  `floor` int(11) DEFAULT NULL,
+  `parents_id` varchar(255) DEFAULT NULL,
+  `commenter` varchar(20) DEFAULT NULL,
+  `comment` varchar(2555) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of blog_comment
+-- ----------------------------
